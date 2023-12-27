9560df273916241a9cc13cbc856469ce15bf8cde
awonder <wonderayanfe0@gmail.com>
2023-12-27 21:14:06 +0100

    Update Attendance and Invitations models, and added useUser store

---
+++ src/utils/useUser.ts
@@ -0,0 +1,30 @@
+import { defineStore } from "pinia";
+
+export const useUser = defineStore({
+    id: 'useUser',
+    state: () => ({
+        users: [{
+            id: '',
+            firstName: '',
+            lastName: '',
+            username: '',
+            email: '',
+            phone: 0,
+            address: '',
+            pasword: ''
+        }]
+    }),
+
+
+
+    actions: {
+        get(id: string) {
+            return this.users.filter(user => user.id === id) 
+        },
+        insert(...user: { id: string; firstName: string; lastName: string; username: string; email: string; phone: number; address: string; pasword: string; }[]) {
+            this.users.push(...user)
+        }
+    }
+
+
+})
